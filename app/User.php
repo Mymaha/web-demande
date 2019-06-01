f311bb6ca614b1669c62388f117db38310b79770
maha <ismahanelakehal13@gmail.com>
2019-06-01 16:17:29 +0200

    add user Agent relationship

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function Agent(){
+        return \App\Models\Agent::where('user_id',$this->id)->first();
+    }
 }
