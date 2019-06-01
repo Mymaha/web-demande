3298cdb6a4fa0a1e168b16035a29c0d43641fc2c
Henni Karam <karam.tenes@gmail.com>
2019-06-01 12:54:07 +0100

    Add demande model and migration

---
+++ database/migrations/2019_06_01_114844_create_demandes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDemandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('demandes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('etat');
+            $table->date('heure_sortie');
+            $table->date('heure_entree');
+            $table->mediumText('motif');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('demandes');
+    }
+}
