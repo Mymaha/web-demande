4cfaebab9b72cdd9820018643f5c471abfda1359
maha <ismahanelakehal13@gmail.com>
2019-06-10 22:10:50 +0200

    'lol'

--- routes/user.php
+++ routes/user.php
@@ -17,3 +17,4 @@ Route::get('/choisir-role','Roles\ChoixController@show')->name('user.role.choix'
 Route::get('/tableau-de-bord','DashboardController@show')->name('user.dashboard');
 Route::get('/demandes/fiche-demande','Demandes\CreateController@show')->name('user.demandes.fiche');
 Route::get('/demandes/{id}/edit-demande','Demandes\EditController@show')->name('user.demandes.edit');
+Route::get('/demandes/{id}/index-demande','Demandes\IndexController@show')->name('user.demandes.index');
