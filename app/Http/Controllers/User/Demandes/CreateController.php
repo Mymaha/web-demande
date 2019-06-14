514cd95e450965b4dc0a2df62db5178424f4e9b7
Henni Karam <karam.tenes@gmail.com>
2019-06-14 18:25:55 +0100

    Add Create demande backend

--- app/Http/Controllers/User/Demandes/CreateController.php
+++ app/Http/Controllers/User/Demandes/CreateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\User\Demandes;
 
+use App\Models\Demande;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,4 +11,21 @@ class CreateController extends Controller
    public function show() {
        return  view('pages.user.demandes.fiche');
 }
+
+	public function create(Request $request){
+
+		$demande = new Demande();
+		$demande->etat = 1;
+		$demande->heure_sortie = $request->heure_sortie;
+		$demande->heure_entree = $request->heure_retour;
+		$demande->date_sortie = $request->date_sortie;
+		$demande->motif = $request->motif;
+		$demande->user_id = auth()->user()->id;
+		$demande->created_at = now();
+		$demande->updated_at = now();
+		$demande->save();
+
+		return redirect(Route('user.demandes'));
+
+	}
 }
--- resources/views/pages/user/demandes/fiche.blade.php
+++ resources/views/pages/user/demandes/fiche.blade.php
@@ -50,20 +50,21 @@
 				<div class=" col-md-4 col-sm-4 col-xs-12">
 
 					<div style="margin-top: 10vh;" class="form-container2">
-						<form>
+						<form method="POST" action="{{Route('user.demandes.fiche')}}">
+              @csrf
 
 						<h4 style="text-align: center; margin-bottom: 5vh;">Autorisation de mouvement externe</h4>
 						 <div class="form-group">
-                          <label class="control-label" for="flatpickr01">Date de sortie :</label> <input id="flatpickr01" type="text" class="form-control" data-toggle="flatpickr">
+                          <label class="control-label" for="flatpickr01">Date de sortie :</label> <input id="flatpickr01" type="text" class="form-control" data-toggle="flatpickr" name="date_sortie">
                         </div>
 						<div class="form-group">
-                          <label class="control-label" for="flatpickr08">Heure de sortie :</label> <input id="flatpickr08" type="text" class="form-control" data-toggle="flatpickr" data-enable-time="true" data-no-calendar="true" data-date-format="H:i" data-default-date="13:45">
+                          <label class="control-label" for="flatpickr08">Heure de sortie :</label> <input id="flatpickr08" type="text" class="form-control" data-toggle="flatpickr" data-enable-time="true" data-no-calendar="true" data-date-format="H:i" data-default-date="13:45" name="heure_sortie">
                         </div>
                         <div class="form-group">
-                          <label class="control-label" for="flatpickr08">Heure de retour :</label> <input id="flatpickr08" type="text" class="form-control" data-toggle="flatpickr" data-enable-time="true" data-no-calendar="true" data-date-format="H:i" data-default-date="13:45">
+                          <label class="control-label" for="flatpickr08">Heure de retour :</label> <input id="flatpickr08" type="text" class="form-control" data-toggle="flatpickr" data-enable-time="true" data-no-calendar="true" data-date-format="H:i" data-default-date="13:45" name="heure_retour">
                         </div>
                         Motif :
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="motif"></textarea>
 
 	                        <div style="display: flex; justify-content: center;" >
 	                        	<button style="width: 25vh; margin-top: 2vh;" class="btn btn-secondary btn-lg" type="submit">
